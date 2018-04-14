675dc81f8e74358734f6a9bbf1c7d44879ef7e73
Mick C <37165418+moneywhore@users.noreply.github.com>
2018-04-13 20:16:08 -0500

    Tried to fix indentation, see how it goes

--- main.c
+++ main.c
@@ -31,7 +31,7 @@ void load_mem(const char *filename[]) {
 
 int fetch()
 {
-  return memory[ pc++ ];
+	return memory[ pc++ ];
 }
 
 int instrNum = 0;
@@ -44,77 +44,79 @@ int sb = 8192;
 
 void decode( int instr )
 {
-  instrNum = (instr & 0xF000) >> 12;
-  reg1     = (instr & 0xF00 ) >>  8;
-  reg2     = (instr & 0xF0  ) >>  4;
-  reg3     = (instr & 0xF   );
-  imm      = (instr & 0xFF  );
+	instrNum = (instr & 0xF000) >> 12;
+	reg1     = (instr & 0xF00 ) >>  8;
+	reg2     = (instr & 0xF0  ) >>  4;
+	reg3     = (instr & 0xF   );
+	imm      = (instr & 0xFF  );
 }
 
 int running = 1;
 
 void eval()
 {
-  switch( instrNum )
-  {
-    case 0:
-      printf( "halt\n" );
-      running = 0;
-      break;
-    case 1:
-      printf( "loadi r%d #%d\n", reg1, imm );
-      regs[ reg1 ] = imm;
-      break;
-    case 2:
-      printf( "add r%d r%d r%d\n", reg1, reg2, reg3 );
-      regs[ reg1 ] = regs[ reg2 ] + regs[ reg3 ];
-      break;
-	case 3:
-	  printf( "push r%d\n", reg1);
-      sp--;
-	  memory[ sp ] = regs[ reg1 ];
-	  printf( "%d\n", sp);
-	  break;
-	case 4:
-	  printf( "pop r%d\n", reg1);
-	  regs[ reg1 ] = memory[ sp ];
-	  memory[ sp ] = 0;
-	  sp++;
-	  printf( "%d\n", sp);
-	  break;
-  }
+	switch( instrNum )
+	{
+		case 0:
+			printf( "halt\n" );
+			running = 0;
+			break;
+		case 1:
+			printf( "loadi r%d #%d\n", reg1, imm );
+			regs[ reg1 ] = imm;
+			break;
+		case 2:
+			printf( "add r%d r%d r%d\n", reg1, reg2, reg3 );
+			regs[ reg1 ] = regs[ reg2 ] + regs[ reg3 ];
+			break;
+		case 3:
+			printf( "push r%d\n", reg1);
+			sp--;
+			memory[ sp ] = regs[ reg1 ];
+			printf( "%d\n", sp);
+			break;
+		case 4:
+			printf( "pop r%d\n", reg1);
+			regs[ reg1 ] = memory[ sp ];
+			memory[ sp ] = 0;
+			sp++;
+			printf( "%d\n", sp);
+			break;
+	}
 }
 
 void showRegs()
 {
-  int i;
-  int x;
-  printf( "regs = " );
-  for( i=0; i<NUM_REGS; i++ )
-    printf( "%04X ", regs[ i ] );
-
-  printf( "\nstack: " );
-  for ( x=1; x<10; x++ ) {
-	printf("%04X ", memory[sb - x]);
-  }
-  printf( "\n" );
+	int i;
+	int x;
+	printf( "regs = " );
+	for( i=0; i<NUM_REGS; i++ )
+	{
+		printf( "%04X ", regs[ i ] );
+	}
+	printf( "\nstack: " );
+	for ( x=1; x<10; x++ ) 
+	{
+		printf("%04X ", memory[sb - x]);
+	}
+	printf( "\n" );
 }
 
 void run()
 {
-  while( running )
-  {
-    showRegs();
-    int instr = fetch();
-    decode( instr );
-    eval();
-  }
-  showRegs();
+	while( running )
+	{
+		showRegs();
+		int instr = fetch();
+		decode( instr );
+		eval();
+	}
+	showRegs();
 }
 
 int main( int argc, const char * argv[] )
 {
-  load_mem(argv);
-  run();
-  return 0;
+	load_mem(argv);
+	run();
+	return 0;
 }
